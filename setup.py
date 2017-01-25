c4f70b39c223a7d96d55776a5ae82285a84d7035
Raghu Rajagopalan <raghu.rajagopalan@gmail.com>
2017-01-25 09:50:42 +0530

    Add README.rst as longdescripton; version bump

--- setup.py
+++ setup.py
@@ -5,8 +5,14 @@ from setuptools import setup
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
 setup(name="EasyBlogger",
-      version="0.9.3",
+      version="0.9.4",
       author="Raghu Rajagopalan",
       author_email="raghu.nospam@gmail.com",
       description=("A (very) easy CLI interface to Blogger blogs"),
@@ -25,7 +31,7 @@ setup(name="EasyBlogger",
               'easyblogger = blogger.blogger:main'
           ]
       },
-      long_description='see https://bitbucket.org/raghur/easyblogger',
+      long_description=readme(),
       classifiers=[
           "Development Status :: 4 - Beta",
           "Topic :: Utilities",
